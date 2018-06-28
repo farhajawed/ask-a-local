9b057911fe6536626da4adc16d89e27adddc8900
farha <jawed.f@husky.neu.edu>
2018-06-27 23:18:24 -0700

    project setup

---
+++ database/schema.sql
@@ -0,0 +1,4 @@
+-- Drops the askLocalDb if it exists currently --
+DROP DATABASE IF EXISTS askLocalDb;
+-- Creates the "askLocalDb" database --
+CREATE DATABASE askLocalDb;
---
+++ routes/html-routes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // index route loads index.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/index.html"));
+  });
+
+};
