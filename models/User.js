03a75df19ed44a4ace828d229d4fd962091f0c29
Farha B. Jawed <farhajw@gmail.com>
2018-06-29 19:49:08 -0700

    init

--- models/User.js
+++ models/User.js
@@ -18,7 +18,7 @@ module.exports =function (sequelize,DataTypes) {
     lastName:DataTypes.STRING,
     bio: DataTypes.TEXT,
     image:
-        {
+    {
             type:DataTypes.STRING
     }
     });
