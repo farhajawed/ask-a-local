cb69415b6581b419289838b3d0da62a9527dad2c
cubanerick <37166192+cubanerick@users.noreply.github.com>
2018-07-02 14:30:04 -0700

    global.html and global.js for global search

--- public/js/global.js
+++ public/js/global.js
@@ -1,3 +1,42 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+$(document).ready(function() {
+    
+    function getPostsbyCat(cat) {
+        var queryUrl = "/api/categories";
+        $.get(queryUrl,function(data){
+            var categories = $(".categories");
+            for(var i = 0; i < data.length; i++){
+                var catbutton = ("<button class ='cat'>" + data[i].name + "</button>")
+                categories.append(catbutton);
+            }
+        })
+    }
+    getPostsbyCat();
+    
+    $(".cat").on("click", (result)=>{
+
+    
+        function writeCatPosts(){
+            var category = $(".cat")
+            var queryUrl = "api/post/categories/" + category;
+            $.get(queryUrl, function(data){
+                console.log(data);
+                for(var i = 0; i < data.length; i++){
+                    var card = (
+                    "<div class='card'>" +
+                        "<img class='card-img-top' src='#' alt='Card image'>" +
+                        "<div class='card-body'>" +
+                            "<h5 class='card-title'>" + username + "</h5>" +
+                            "<h6 class='card-subtitle mb-2 text-muted'>" + cardtitle + "</h6>" +
+                            "<p class='card-text'>" + body + "</p>" +
+                        "</div>" + 
+                    "</div>")
+                    $.get("api/posts/" + + "/comments", function(data){
+                        console.log(data);
+
+                    });
+                }
+            })
+        }
+
+    });
+});
