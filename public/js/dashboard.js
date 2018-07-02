28c65f0f0eb93c53c6e3b8f7aeb597059a92a581
farha <jawed.f@husky.neu.edu>
2018-07-02 03:12:36 -0700

    dashboard shows posts

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
     var aboutDiv = $(".about-div");
+    var postContainer = $(".post-container");
 
-    $("#profile-form").on("submit",handleLogin);
-
-    function handleLogin(){
-        console.log($('input[type=file]').val());
-    }
- 
-    
     getLoggedUser();
     function getLoggedUser(){
         $.get("/user",function(data){
-            $(".logged-username").html(data.username);   
+            // $(".logged-username").html(data.username);   
+            getUserInfo(data.id);
+            getPostData(data.id);
+        });
+    }
+    
+    function getUserInfo(id){
+        var queryUrl = "/user/" + id;
+        $.get(queryUrl, function(data) {
             showAboutSection(data);
         });
     }
     
-    function showAboutSection(user){
 
+    function showAboutSection(user){
+        $(".edit-about").data("user",user);
         // image
         $(".profile-image").attr("src","/images/upload_images/"+user.image);
         
@@ -26,33 +29,109 @@ $(document).ready(function() {
         var fullName = p.html(user.firstName+" "+user.lastName);
         aboutDiv.append(fullName);
         }
-        //    //location
-        //    var img = $("<img>").attr("src","/images/location.png");
-        //    var text = "Lives in : Boston, MA";
-        //    var location = $("<p>").append(img,text); 
-
+        //bio
+        if(user.bio){
+            var text = '"'+user.bio+'"';
+            var bioP = $("<p>").append(img,text).addClass("text-center bio"); 
+            aboutDiv.append(bioP);
+        }
+        //location
+        if(user.location){ 
+            var img = $("<img>").attr("src","/images/location.png");
+            var text = "Lives in : "+user.location;
+            var locationP = $("<p>").append(img,text); 
+            aboutDiv.append(locationP);
+        }
+        
         // email
         var img = $("<img>").attr("src","/images/email.png");
-        var text = "Email: "+user.email;
+        var text = "Email : "+user.email;
         var emailP = $("<p>").append(img,text);
         aboutDiv.append(emailP);
 
         // membership data
         var img = $("<img>").attr("src","/images/member.png");
         var randomDate = user.createdAt;
-        var randomFormat = "YYYY-MM-DDThh:mm.sssZ";
-        var convertedDate = moment(randomDate, randomFormat);
-        var modifiedDate = moment(convertedDate).format("MM/DD/YYYY");
-        var text = "Member Since: "+modifiedDate;
+        var text = "Member Since : "+dateFormat(randomDate,"MM/DD/YYYY");
         var memberP = $("<p>").append(img,text);
         aboutDiv.append(memberP);
         //total posts
         // var img = $("<img>").attr("src","/images/post.png");
         // var text = "Total Posts: 0";
         // var postCount = $("<p>").append(img,text);
-        
-           
-
+        $(".edit-about").on("click",populateData);
     };
 
+    function dateFormat(randomDate,form){
+        var randomFormat = "YYYY-MM-DDThh:mm.sssZ";
+        var convertedDate = moment(randomDate, randomFormat);
+        var modifiedDate = moment(convertedDate).format(form);
+        return modifiedDate;
+    }
+
+    function populateData(){
+        $("#firstName").val($(this).data("user").firstName);
+        $("#lastName").val($(this).data("user").lastName);
+        $("#location").val($(this).data("user").location);
+        $("#bio").val($(this).data("user").bio);
+        // $('input[type=file]#profile-image').val($(this).data("user").image);
+    }
+
+    
+  function getPostData(id) {
+    var queryUrl = "/api/posts/user/" + id;
+    $.get(queryUrl, function(data) {
+    
+     var postDiv =$("<div>").addClass("post-div mb-4");
+     var singlePostDiv = $("<div>").addClass("single-post");
+     var postTitle = $("<div>").addClass("post-title");
+
+      if (data.length<1) {
+         console.log("no posts");
+         postTitle.html("No posts to display. Click on 'Make a post' icon on menu to create a new post.");
+         postContainer.append(postDiv.append(singlePostDiv.append(postTitle)));
+        
+      }
+      else{
+          console.log(data);
+          postContainer.empty();
+          var postsToAdd = [];
+          for(var i=0; i<data.length;i++){
+            postsToAdd.push(createNewRow(data[i]));
+          }
+          postContainer.append(postsToAdd);
+          
+      }
+     
+    });
+  }
+
+  function createNewRow(post){
+       
+     var postDiv =$("<div>").addClass("post-div mb-4");
+     var singlePostDiv = $("<div>").addClass("single-post");
+     var postTitle = $("<div>").addClass("post-title");
+     postTitle.html(post.title);
+     var additionalDiv = $("<div>").addClass("additional-div");
+     var text = "Posted in "+post.Category.name+" | Created at "+dateFormat(post.createdAt,"MM/DD/YYYY hh:mm:ss");
+     additionalDiv.html(text);
+     postDiv.append(singlePostDiv.append(postTitle,additionalDiv));
+     return postDiv;
+  }
+
+
 });
+
+//  <div class="post-div mb-4">
+//         <div class="single-post">
+//             <div class="post-title">
+//                 “A few of my favorites are: blue crab hand roll, baked mussel,
+//             oyster, salmon, freshwater eel, tomago, albacore, and scallop.” 
+//             </div>
+//             <span class="view-edit-btn"><i class="far fa-eye view-btn" data-toggle="tooltip" data-placement="bottom" title="View post"></i>
+//                                     <i class="far fa-edit edit-btn" data-toggle="tooltip" data-placement="bottom" title="Edit post"></i></span>
+        
+//             <div class="additional-div">Posted in Genereal | 0 comments | Created at 23 March,2018</div>
+//     </div>      
+//     </div> 
+// </div> 
--- public/js/post.js
+++ public/js/post.js
@@ -1,8 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
   var url = window.location.href;
-  console.log("u");
-  console.log(url);
- 
+
   if (url.indexOf("?post_id=") !== -1) {
     var postId = url.split("=")[1];
     getPostData(postId);
@@ -12,47 +11,56 @@ $(document).ready(function() {
     // getPosts();
   }
 
-  if (url.indexOf("?email=") !== -1) {
-    var userEmail = url.split("=")[1];
-    console.log(userEmail);
-    // getUserEmail(userEmail);
-    $("#userEmail").html(userEmail);
-  }
- 
-
- function getCategories() {
-    $.get("/api/categories", function(data) {
+  function getCategories() {
+    $.get("/api/categories", function (data) {
       if (data) {
-         var categoryContainer = $("#category");
-         for(var i = 0; i<data.length; i++){
-             var option = $("<option>");
-             option.attr("value",i+1);
-             option.html(data[i].name);
-             categoryContainer.append(option);
-         }
+        var categoryContainer = $("#category");
+        var option = $("<option>");
+        option.attr("value", "default");
+        option.attr("disabled", "disabled");
+        option.attr("selected", "selected");
+        option.html("Select a category of your post");
+        categoryContainer.append(option);
+        for (var i = 0; i < data.length; i++) {
+          var option = $("<option>");
+          option.attr("value", i + 1);
+          option.html(data[i].name);
+          categoryContainer.append(option);
+        }
       }
     });
   }
-  
+
   getCategories();
- 
+
 
   function getPostData(id) {
     var queryUrl = "/api/posts/" + id;
-    $.get(queryUrl, function(data) {
+    $.get(queryUrl, function (data) {
       if (data) {
-         console.log(data);
-         $("#post-title").append(data.title);
-         $("#post-body").append(data.body);
-         $("#post-category").append(data.Category.name);
+        console.log(data);
+        $("#post-title").append(data.title);
+        $("#post-body").append(data.body);
+        $("#post-category").append(data.Category.name);
         //  if(data.image){
-          var img = $("<img>").attr("src","/images/upload_images/"+data.image);
-          img.addClass("img-style");
-          $("#post-image").append(img); 
-        //  }    
+        var img = $("<img>").attr("src", "/images/upload_images/" + data.image);
+        img.addClass("img-style");
+        $("#post-image").append(img);
+        $("#post-date").append(data.createdAt);
+        getAuthorInfo(data.UserId);
       }
     });
   }
-  
- 
+
+  function getAuthorInfo(id) {
+    var queryUrl = "/user/" + id;
+    $.get(queryUrl, function (data) {
+      if (data) {
+        console.log(data);
+        $("#post-author").append(data.username);
+      }
+    })
+  }
+
+
 });
